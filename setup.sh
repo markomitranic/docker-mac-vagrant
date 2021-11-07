b55dd600ef5b17cd283b7be9a7bf6b53c514f585
Boris Ćeranić <boris.ceranic@catenamedia.com>
2021-11-07 22:43:36 +0100

    Revert "Convert .env file provisioning to manual step" (#11)
    * Revert "Convert .env file provisioning to manual step"
    
    * Add .env file contents preview into setup.sh
    
    This should make it obvious to users that .env file is there for them
    to be customised if they disagree with our defaults.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,18 @@
 brew install vagrant
 vagrant plugin install vagrant-parallels
 vagrant plugin install vagrant-env
+
+if [ ! -f .env ]; then
+    cp .env.dist .env
+fi
+
+echo
+echo 'Contents of your .env file:'
+
+cat .env
+
+echo
+echo 'Please review variables defined in .env file shown above.'
+echo 'If you need to make any adjustments, edit .env file now!'
+echo 'For example, you might want to adjust SHARE_PATH to where'
+echo 'your work directory resides.'
