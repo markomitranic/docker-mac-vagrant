723db28f59535ce1df5d46b8b6e5af6163f6976f
Marko Mitranić <marko.mitranic@gmail.com>
2021-12-29 16:05:14 +0000

    Fix a broken compose version

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y unzip git ncdu
 
 # Install docker-compose
 # https://docs.docker.com/compose/install/#install-compose-on-linux-systems
-curl -L "https://github.com/docker/compose/releases/download/2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
